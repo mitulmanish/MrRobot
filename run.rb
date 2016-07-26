bfa241369de6222c14118bef64c7ec39090a3ca1
Mitul Manish <mitul.manish@gmail.com>
2016-07-26 12:13:32 +1000

    adding read and run files

---
+++ run.rb
@@ -0,0 +1,5 @@
+require_relative 'lib/simulator'
+# default size of the matrix is set to 5.Although program works just fine with any size.
+SIZE = 5
+simulator = Simulator.new(SIZE)
+simulator.get_user_input
